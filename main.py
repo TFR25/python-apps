c4ca981100cda6db4845e3e4f7e46dea85a9f91a
Tasha Fernandez-Ross <tfernandez25@gmail.com>
2024-05-15 08:52:57 -0700

    create footer.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
+from datetime import datetime
+
 
 app = Flask(__name__)
-
-app.route("/")
 Bootstrap5(app)
+
+now = datetime.now()
+current_year = now.year
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template('index.html', year=current_year)
 
 
 if __name__ == "__main__":
