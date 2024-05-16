15a911b40ac9866ba651e338eb7022e08b9267d9
Tasha Fernandez-Ross <tfernandez25@gmail.com>
2024-05-15 17:44:21 -0700

    add contact form.

--- main.py
+++ main.py
@@ -1,18 +1,68 @@
-from flask import Flask, render_template
+import json
+import smtplib
+
+from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap5
 from datetime import datetime
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, validators
+import os
 
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
 Bootstrap5(app)
 
 now = datetime.now()
 current_year = now.year
 
 
+class contactForm(FlaskForm):
+    name = StringField('Name', [validators.InputRequired()])
+    email = StringField('Email', [validators.InputRequired()])
+    message = TextAreaField('Message', [validators.InputRequired()])
+    submit = SubmitField('Send')
+
+
+my_email = "my email"
+my_pwd = "my email password"
+
+try:
+    with open("data.json", "r") as data_file:
+        data = json.load(data_file)
+
+except FileNotFoundError:
+    print("Error, file not located.")
+except json.decoder.JSONDecodeError:
+    print("Error, file empty.")
+else:
+    my_email = data["gmail"]["email"]
+    my_pwd = data["gmail"]["password"]
+
+
 @app.route("/")
 def home():
     return render_template('index.html', year=current_year)
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    cForm = contactForm()
+    try:
+        if cForm.validate_on_submit():
+            with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+                connection.starttls()
+                connection.login(user=my_email, password=my_pwd)
+                connection.sendmail(
+                    from_addr=cForm.email.data,
+                    to_addrs=my_email,
+                    msg=f"Subject: Portfolio contact form \n\nName: {cForm.name.data},\n\nEmail: {cForm.email.data}, \n\n{cForm.message.data}")
+            cForm = contactForm(formdata=None)
+            flash("Thank you. Message sent successfully.")
+    except UnicodeEncodeError:
+        flash("Sorry emoji's not supported. Message not sent.")
+        return render_template('contact.html', form=cForm, year=current_year)
+    else:
+        return render_template('contact.html', form=cForm, year=current_year)
 
 
 if __name__ == "__main__":
--- static/javascript/index.js
+++ static/javascript/index.js
@@ -5,12 +5,6 @@ bar.addEventListener("click", () => {
   menu.classList.toggle("show-menu");
   bar.classList.toggle("fa-bars-transparent")
 });
-
-/*!
-* Start Bootstrap - Clean Blog v6.0.9 (https://startbootstrap.com/theme/clean-blog)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
-*/
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
     const mainNav = document.getElementById('mainNav');
